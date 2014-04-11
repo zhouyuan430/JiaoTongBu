6cd611bae7d5ea1a7944a3ccfcb609bc713132b1
zhouyuan <zhou_yuan1993@163.com>
2014-04-11 11:42:21 +0800

    个人资产界面 资产详情界面 相册或照相选择图片 公共类用与资产列表

---
+++ Commen/AssetCell.h
@@ -0,0 +1,13 @@
+//
+//  AssetCell.h
+//  JiaoTongBu
+//
+//  Created by zhouyuan on 14-4-11.
+//  Copyright (c) 2014年 zhouyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AssetCell : UITableViewCell
+
+@end
---
+++ PersonalAssets/PersonalAssetsViewController.h
@@ -0,0 +1,13 @@
+//
+//  PersonalAssetsViewController.h
+//  JiaoTongBu
+//
+//  Created by zhouyuan on 14-4-11.
+//  Copyright (c) 2014年 zhouyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PersonalAssetsViewController : UITableViewController
+
+@end
